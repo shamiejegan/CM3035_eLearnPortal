8f9b16fba6ea177668c9cddaa424ba15fefdc020
Shamie J <shamiejegan@gmail.com>
2024-02-16 22:42:09 +0800

    configure admin

--- elearn_app/admin.py
+++ elearn_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register all models  
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
