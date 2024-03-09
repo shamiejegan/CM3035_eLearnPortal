b30857b03c9b9c16c9e15258d377c454b51ce70c
Shamie Jegan <shamiejegan@Shamies-MacBook-Pro.local>
2024-03-09 23:35:15 +0800

    before moving chat to course page

--- chat/consumers.py
+++ chat/consumers.py
@@ -23,18 +23,26 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sender_username = text_data_json['sender']
+        auth_group = text_data_json['auth_group']
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sender': sender_username,
+                'auth_group': auth_group
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
+        sender_username = event['sender']
+        auth_group = event['auth_group']
 
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'sender': sender_username,
+            'auth_group': auth_group
         }))
--- elearn_app/static/js/chat.js
+++ elearn_app/static/js/chat.js
@@ -5,14 +5,24 @@ const auth_group = JSON.parse(document.getElementById('auth_group').textContent)
 const chatSocket = new WebSocket('ws://'+ window.location.host+ '/ws/'+roomName+ '/'
 );
 
+chatSocket.onopen = function(e) {
+    chatSocket.send(JSON.stringify({
+        'message': 'Joined room',
+        'sender': username, // Include sender's username
+        'auth_group': auth_group // Include sender's auth group
+    }));
+};
 chatSocket.onmessage = function(e) {
     const data = JSON.parse(e.data);
-    document.querySelector('#chat-log').value += (username + ' ('+auth_group+'): ' + data.message + '\n');
+    const chatLog = document.querySelector('#chat-log');
+    chatLog.value += (data.sender + ' (' + data.auth_group + '): ' + data.message + '\n'); 
+    chatLog.scrollTop = chatLog.scrollHeight;
 };
 
 chatSocket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
 };
+
 
 document.querySelector('#chat-message-input').focus();
 document.querySelector('#chat-message-input').onkeyup = function(e) {
@@ -25,7 +35,9 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
     const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
     chatSocket.send(JSON.stringify({
-        'message': message
+        'message': message,
+        'sender': username, // Include sender's username
+        'auth_group': auth_group // Include sender's auth group
     }));
     messageInputDom.value = '';
 };
